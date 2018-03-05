fdd8442137fa64de461bc9b1e593bd4fad61b0a0
YessMG <37080294+YessMG@users.noreply.github.com>
2018-03-05 10:57:57 -0600

    Add files via upload

---
+++ libros2017-03-22 20-22-53.sql
@@ -0,0 +1,12 @@
+USE inventario;
+
+CREATE TABLE `libros` (
+  `titulo` varchar(20) DEFAULT NULL,
+  `editorial` varchar(20) DEFAULT NULL,
+  `autor` varchar(20) DEFAULT NULL,
+  `paginas` varchar(20) DEFAULT NULL,
+  `genero` varchar(20) DEFAULT NULL,
+  `idioma` varchar(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
